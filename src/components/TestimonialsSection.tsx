178331539cee6c868d151246d5554d0f1b108861
Usama Khursheed <hyperloomofficial@gmail.com>
2026-09-18 11:26:17 +0500

    Optimize responsive grid columns for 11 video review cards in TestimonialsSection

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -207,7 +207,7 @@ export const TestimonialsSection: React.FC = () => {
         </div>
 
         {/* Video Reviews Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {videoReviews.map((rev) => (
             <div
               key={rev.id}
